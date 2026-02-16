3c2224b0f1330161bb33c2f2c1e4db65c1e34b79
Dan <87450103+zomberdaniel@users.noreply.github.com>
2026-02-16 17:07:08 +0100

    feat(vm): Přidána Public IP

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,3 +1,12 @@
+resource "azurerm_public_ip" "this" {
+  name                = "${var.vm_name}-pip"
+  location            = var.location
+  resource_group_name = var.rg_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = var.tags
+}
+
 resource "azurerm_network_interface" "this" {
   name                = "${var.vm_name}-nic"
   location            = var.location
@@ -7,6 +16,7 @@ resource "azurerm_network_interface" "this" {
     name                          = "internal"
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.this.id
   }
 
   tags = var.tags
